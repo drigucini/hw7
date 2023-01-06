2ac83ba4c8bf401416f189729334cf5e3b315574
Sergei Driga <drigaserge@gmail.com>
2023-01-06 17:24:33 +0300

    hw7 initial commit

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+        task8();
+    }
+
+    public static void task1 () {
+        System.out.println("Task 1");
+    }
+
+    public static void task2 () {
+        System.out.println("\nTask 2");
+    }
+
+    public static void task3 () {
+        System.out.println("\nTask 3");
+    }
+
+    public static void task4 () {
+        System.out.println("\nTask 4");
+    }
+
+    public static void task5 () {
+        System.out.println("\nTask 5");
+    }
+
+    public static void task6 () {
+        System.out.println("\nTask 6");
+    }
+
+    public static void task7 () {
+        System.out.println("\nTask 7");
+    }
+
+    public static void task8 () {
+        System.out.println("\nTask 8");
+    }
+}
